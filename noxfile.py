2e184782b03802e3985421f65435d59a651ae6cb
Jonathan Zarecki <yonilx@gmail.com>
2022-02-28 19:57:26 +0200

    fix: remove modules that made the ci crash
    doc8 and other

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,7 @@ import toml
 from nox import Session
 
 package = "dc_comp"
-python_versions = ["3.7"]
+python_versions = ["3.8"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = ("tests", "xdoctest")  # , "pre-commit"
 pyproject_data = toml.loads(Path("pyproject.toml").read_text())
@@ -24,8 +24,22 @@ if os.path.exists(".gitmodules"):
 def pre_commit(sess: Session) -> None:
     """Run pre-commit on all files."""
     sess.install("pre-commit")
-    sess.run(*"pre-commit install --install-hooks -t pre-commit -t commit-msg -t post-commit -t pre-push".split(" "))
-    sess.run(*"pre-commit run --all-files".split(" "))
+    sess.run(
+        *[
+            "pre-commit",
+            "install",
+            "--install-hooks",
+            "-t",
+            "pre-commit",
+            "-t",
+            "commit-msg",
+            "-t",
+            "post-commit",
+            "-t",
+            "pre-push",
+        ]
+    )
+    sess.run(*["pre-commit", "run", "--all-files"])
 
 
 @nox.session(python=False)
@@ -45,19 +59,6 @@ def tests(sess: Session) -> None:
         sess.run("coverage", "run", "--parallel", *omit_paths, "-m", "pytest", *run_paths, *sess.posargs)
     finally:
         sess.notify("coverage", posargs=[])
-
-
-@nox.session(python=python_versions)
-def coverage(sess: Session) -> None:
-    """Produce the coverage report."""
-    args = sess.posargs or ["report"]
-
-    sess.install("coverage[toml]")
-
-    if not sess.posargs and any(Path().glob(".cache/.coverage.*")):
-        # keep .coverage.* files if not interactive (i.e. CI)
-        sess.run(*(["coverage", "combine"] + (["--keep"] if not sess.interactive else [])))
-    sess.run("coverage", *args)
 
 
 @nox.session(python=False)
