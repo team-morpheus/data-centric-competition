2c9bc0199cb6ea526083dd8a9c7de08419e8f380
Jonathan Zarecki <yonilx@gmail.com>
2022-03-14 09:57:42 +0200

    feat: improve photo slider to use folders instead of tile datasets.
    .

--- common/visualizations/photo_slider.py
+++ common/visualizations/photo_slider.py
@@ -1,11 +1,11 @@
 import os
+import shutil
 from typing import List
 
 from bokeh.layouts import column, row
 from bokeh.models import ColumnDataSource, CustomJS, Slider
 from bokeh.models.glyphs import ImageURL
 from bokeh.plotting import figure, show
-from tqdm import trange
 
 
 def _disable_all_for_pictures(p: figure) -> figure:  # pylint: disable=C0116
@@ -24,59 +24,39 @@ def _disable_all_for_pictures(p: figure) -> figure:  # pylint: disable=C0116
     return p
 
 
-def _save_tile_images_to_local_path(_photos_dir: str, max_photos: int = 1000) -> List[str]:  # pylint: disable=C0116
+def _save_tile_images_to_local_path(_photos_dir: str, max_photos: int) -> List[str]:  # pylint: disable=C0116
     # Save folders in curr folder for bokeh access
     os.makedirs("pics/", exist_ok=True)
-    a_img_paths = []
-    for i in trange(min(4, max_photos), desc="loading tiles"):
-        # img_a = None[i][0][0]  # noqa
+    orig_img_paths = [os.path.join(_photos_dir, fname) for fname in os.listdir(_photos_dir)][:max_photos]
+    copied_img_paths = [os.path.join("pics/", fname) for fname in os.listdir(_photos_dir)][:max_photos]
+    for copied_im_p, orig_im_p in zip(copied_img_paths, orig_img_paths):
+        shutil.copy(orig_im_p, copied_im_p)
 
-        a_img_p = f"pics/{i}-A.png"
-
-        # def save_to_png(img_channel, p):
-        #     data = np.random.randint(5, size=(224, 224), dtype=np.uint8)
-        #
-        #     data[...] = img_channel.numpy() * 255
-        #     # rgbArray[..., 0] = img_channel.numpy() * 255
-        #     # rgbArray[..., 2] = img_channel.numpy() * 255
-        #     Image.fromarray(data).save(p)
-        #
-        # save_to_png(img_a_p, a_img_p)  # noqa
-
-        a_img_paths.append(a_img_p)
-
-    return a_img_paths
+    return copied_img_paths
 
 
-def multi_channel_tile_slider() -> None:
-    """View interactively with bokeh the 3 image tiles."""
-    n = 2
-    # a_img_paths, b_img_paths, c_img_paths = _save_tile_images_to_local_path(dataset, n)
-
-    img_paths_orig = [
-        "pics\\1-A.png",
-        "pics\\2-A.png",
-    ]
+def multi_channel_tile_slider(img_folder_path: str, max_photos: int = 100) -> None:
+    """View interactively with bokeh the images."""
+    c_img_paths = _save_tile_images_to_local_path(img_folder_path, max_photos)
+    n = len(c_img_paths)
 
     # the plotting code
     plots = []
     sources = []
-    paths = [img_paths_orig, img_paths_orig, img_paths_orig]
-    plot_num = 3
+    plot_num = 1
 
-    for i in range(plot_num):
-        p = figure(height=300, width=300)
-        img_paths = paths[i]
-        # print(img_paths)
-        source = ColumnDataSource(
-            data=dict(url=[img_paths_orig[0]] * n, url_orig=img_paths, x=[1] * n, y=[1] * n, w=[1] * n, h=[1] * n)
-        )
-        image = ImageURL(url="url", x="x", y="y", w="w", h="h", anchor="bottom_left")
-        p.add_glyph(source, glyph=image)
-        _disable_all_for_pictures(p)
+    p = figure(height=300, width=300)
+    # img_paths = paths[i]
+    # print(img_paths)
+    source = ColumnDataSource(
+        data=dict(url=[c_img_paths[0]] * n, url_orig=c_img_paths, x=[1] * n, y=[1] * n, w=[1] * n, h=[1] * n)
+    )
+    image = ImageURL(url="url", x="x", y="y", w="w", h="h", anchor="bottom_left")
+    p.add_glyph(source, glyph=image)
+    _disable_all_for_pictures(p)
 
-        plots.append(p)
-        sources.append(source)
+    plots.append(p)
+    sources.append(source)
 
     update_source_str = """
         var data = source{i}.data;
@@ -92,7 +72,7 @@ def multi_channel_tile_slider() -> None:
     """
     # the callback
     callback = CustomJS(
-        args=dict(source0=sources[0], source1=sources[1], source2=sources[2]),
+        args=dict(source0=sources[0]),
         code=f"""
         var f = cb_obj.value;
         console.log(f);
